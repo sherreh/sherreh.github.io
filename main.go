2ded719616b914ce55b02eed549a1ab271c23ced
sherreh <>
2021-07-08 23:48:21 +0100

    clock

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "github.com/gopherjs/gopherjs/js"
+import (
+    "github.com/gopherjs/gopherjs/js"
+    //"log"
+)
+
+var date *js.Object
+var h1 *js.Object
+
+func fn() {
+    h1.Set("innerHTML", date.New())
+}
 
 func main() {
-    js.Global.Call("alert", "Hello from Go")
+    doc := js.Global.Get("document")
+    h1 = doc.Call("createElement", "h1")
+    date = js.Global.Get("Date")
+    h1.Set("innerHTML", date.New())
+    h1.Set("id", "clock")
+    doc.Get("body").Call("appendChild", h1)
+    js.Global.Call("setInterval", fn, 1000)
 }
