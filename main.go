d99fa6d93869a755a4431753175b6811f240ddfc
sherreh <>
2021-07-09 01:06:28 +0100

    hello world

--- main.go
+++ main.go
@@ -3,18 +3,23 @@ package main
 import (
     "github.com/gopherjs/gopherjs/js"
     "log"
+    "time"
+    "strconv"
 )
 
 var doc, win, canvas, ctx *js.Object
 var width, height int
+
+func clearCanvas() {
+    ctx.Call("clearRect", 0, 0, width, height)
+}
 
 func fitCanvas() {
     width = win.Get("innerWidth").Int()
     height = win.Get("innerHeight").Int()
     canvas.Set("width", width)
     canvas.Set("height", height)
-    ctx.Set("fillStyle", "black")
-    ctx.Call("fillRect", 0, 0, width, height)
+    clearCanvas()
 }
 
 func handleResize(event *js.Object) {
@@ -28,11 +33,24 @@ func initGfx() {
     ctx = canvas.Call("getContext", "2d")
     win = js.Global.Get("window")
     win.Call("addEventListener", "resize", handleResize)
-    doc.Get("body").Set("style", "background-color:grey;touch-action:none;-webkit-overflow-scrolling:none;overflow:hidden;overscroll-behavior:none;padding:0;margin:0")
+    doc.Get("body").Set("style", "background-color:black;touch-action:none;-webkit-overflow-scrolling:none;overflow:hidden;overscroll-behavior:none;padding:0;margin:0")
     doc.Get("body").Call("appendChild", canvas)
     fitCanvas()
 }
 
 func main() {
     initGfx()
+    ctx.Set("fillStyle", "white")
+    for i := 0;; i++ {
+        clearCanvas()
+        ctx.Set("font", strconv.Itoa(25+i)+"px Monospace")
+        textSize := ctx.Call("measureText", "Hello world")
+        textWidth := textSize.Get("width").Int()
+        textHeight := textSize.Get("height").Int()
+        ctx.Call("fillText", "Hello world", width/2-textWidth/2, height/2-textHeight/2)
+        if textWidth >= int(0.7*float64(width)) || textHeight >= int(0.7*float64(height)) {
+            break
+        }
+        time.Sleep(time.Millisecond*50)
+    }
 }
